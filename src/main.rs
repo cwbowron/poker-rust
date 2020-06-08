e443f94b630f15cbc9bbc48d2ba6c3b31cb0d4c6
Christopher Bowron <cwbowron@gmail.com>
2020-06-08 09:09:32 -0400

    Convert main to use `make_poker_hand`

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod deck;
 use deck::make_shuffled_deck;
 
 mod poker_hand;
-use poker_hand::PokerHand;
+use poker_hand::make_poker_hand;
 
 fn deal(cards: &mut Vec<Card>, n: usize) {
     let mut pockets = Vec::new();
@@ -45,12 +45,12 @@ fn deal(cards: &mut Vec<Card>, n: usize) {
         let mut cards = pocket.to_vec();
         cards.extend(board.to_vec());
 
-        let poker_hand = PokerHand::new(&cards);
+        let poker_hand = make_poker_hand(&cards, &None);
         evals.push((pocket, poker_hand));
     }
 
-    evals.sort_by(|a, b| a.1.cmp(&b.1));
-    evals.reverse();
+    // evals.sort_by(|a, b| a.1.cmp(&b.1));
+    // evals.reverse();
 
     for eval in evals {
         let (pocket, poker_hand) = eval;
